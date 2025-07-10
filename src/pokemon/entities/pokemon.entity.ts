0dc1defaa325574699271052c82f75e97eea3fb6
ovidio <ovijar@gmail.com>
2025-07-10 12:26:06 -0500

    proyecto pokemon levantado

---
+++ src/pokemon/entities/pokemon.entity.ts
@@ -0,0 +1,21 @@
+import { Schema, Prop, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+@Schema()
+export class Pokemon extends Document{
+
+    //id: string; //mongoose lo genera automaticamente
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    name: string;
+
+    @Prop({
+        unique: true,
+        index: true,
+    })
+    no: number;
+
+}
+
+export const PokemonSchema = SchemaFactory.createForClass(Pokemon);
